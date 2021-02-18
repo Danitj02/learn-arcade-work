3e3911d67bc4dee7b07f6a77ca934a04d079cb9b
Danitj02 <daniel.santosb@edu.uah.es>
2021-02-18 20:20:23 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,4 @@
-print('Hello World')
-print("It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
+print("On Monday morning I went running in the park. I ran 7 kilometers, and then came home.")
+print("Tomorrow I'll make lunch, chicken whit honey to be more specific. Chinesse food.")
+print("- To buy list -:\nMilk \nCereals \nBread")
+print("Yesterday I spent the evening watching TV on the sofa.")
